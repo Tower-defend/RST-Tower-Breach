8a6a5c6170e1d644719a2e8702b716a30d96658d
TonySintu <71342964+TonySintu@users.noreply.github.com>
2020-10-27 13:38:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import pygame
+import random
+import math
 
-//test YAY
+pygame.init()
+
+screen = pygame.display.set_mode((699,519))
+background = pygame.image.load("map2.png")
+pygame.display.set_caption("Tower Defense")
+icon = pygame.image.load("slime.png")
+pygame.display.set_icon(icon)
+running = True
+
+
+
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
